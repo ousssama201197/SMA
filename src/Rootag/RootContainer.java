a81601d4bdb27d8a53fa0f6b499dfa5727af1a32
oussama <ousssama201197@gmail.com>
2020-02-25 14:33:53 +0100

    Root

--- src/Rootag/RootContainer.java
+++ src/Rootag/RootContainer.java
@@ -1,5 +1,8 @@
 package Rootag;
 
+import Entities.Utilisateur;
+import Utilisateur.UtilisateurContainer;
+import interfaceGraphique.inscription;
 import jade.util.leap.Properties;
 import jade.util.ExtendedProperties;
 import jade.core.ProfileImpl;
@@ -9,34 +12,48 @@ import jade.gui.GuiEvent;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
+import java.util.ArrayList;
 
-public class RootContainer  implements Runnable{
+public class RootContainer implements Runnable {
+
+    inscription fenetre;
+    ArrayList<Utilisateur> users;
+    ArrayList<String> centreInt;
 
     public RootContainer() {
-
     }
 
     @Override
     public void run() {
         try {
-            
+
             Runtime rt = Runtime.instance();
             Properties p = new ExtendedProperties();
             p.setProperty(Profile.MAIN_HOST, "localhost");
             Profile profile = new ProfileImpl(p);
             AgentContainer container = rt.createAgentContainer(profile);
             AgentController Agent = null;
-            Agent = container.createNewAgent("Root" , "Rootag.Root", new Object[]{this});
+            Agent = container.createNewAgent("Root", "Rootag.Root", new Object[]{this});
             Agent.start();
             container.start();
+
+            // initialisation 
+            centreInt = new ArrayList<String>();
+            centreInt.add("informatique");
+            centreInt.add("Sport");
+
+            fenetre = new inscription(this, centreInt);
+            users = new ArrayList<Utilisateur>();
 
         } catch (ControllerException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
-   
-  
+    
+    public void autentifier(Utilisateur user) {
+         users.add(user);
+         new Thread((Runnable) new UtilisateurContainer(4)).start();
+    }
 
 }
